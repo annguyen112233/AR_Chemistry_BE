0af8fa5c87b21bb517f9a80d13bee02d00384b1a
Nguyen Hoai An <annguyen@gmail.com>
2026-05-20 07:52:20 +0700

    [REFACTOR: refactor api synce]

--- src/main/java/com/chemistry/demo/services/user/impl/UserServiceImpl.java
+++ src/main/java/com/chemistry/demo/services/user/impl/UserServiceImpl.java
@@ -3,9 +3,13 @@ package com.chemistry.demo.services.user.impl;
 import com.chemistry.demo.dto.request.UpdateProfileRequest;
 import com.chemistry.demo.dto.response.UpdateProfileResponse;
 import com.chemistry.demo.dto.response.UserResponse;
+import com.chemistry.demo.entity.Role;
 import com.chemistry.demo.entity.User;
+import com.chemistry.demo.enums.RoleName;
 import com.chemistry.demo.mapper.UserMapper;
+import com.chemistry.demo.repository.RoleRepository;
 import com.chemistry.demo.repository.UserRepository;
+import com.chemistry.demo.services.aws.CognitoService;
 import com.chemistry.demo.services.user.UserService;
 import com.chemistry.demo.utils.SecurityUtils;
 import lombok.RequiredArgsConstructor;
@@ -13,28 +17,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
     private final SecurityUtils securityUtils;
+    private final CognitoService cognitoService;
 
     @Override
     @Transactional
-    public UserResponse syncUser(String email,String cognitoSub) {
+    public UserResponse syncUser(
+            String email,
+            String cognitoUsername,
+            String cognitoSub
+    ) {
+
+        AtomicBoolean isNewUser = new AtomicBoolean(false);
+
+        Role studentRole = roleRepository.findByRoleName(RoleName.ROLE_STUDENT)
+                .orElseThrow(() -> new RuntimeException("Role ADMIN not found"));
 
         User user = userRepository.findByCognitoSub(cognitoSub)
                 .orElseGet(() -> {
-                    User newUser = User.builder()
+                    isNewUser.set(true);
+                    return User.builder()
                             .cognitoSub(cognitoSub)
                             .email(email)
-                            .build();
-                    return userRepository.save(newUser);
+                            .roles(new java.util.HashSet<>(java.util.List.of(studentRole)))
+                        .build();
+
                 });
+
+
+        user = userRepository.save(user);
+        if (isNewUser.get()) {
+
+            cognitoService
+                    .addUserToGroup(
+                            cognitoUsername,
+                            RoleName.ROLE_STUDENT.name()
+                    );
+        }
+
         return userMapper.toUserResponse(user);
     }
 
