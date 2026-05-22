dd142c254a7f600277293de5a003164d0dcdbd7c
Nguyen Hoai An <annguyen@gmail.com>
2026-05-22 10:55:33 +0700

    [Feat]: add subscription

--- src/main/java/com/chemistry/demo/config/SecurityConfig.java
+++ src/main/java/com/chemistry/demo/config/SecurityConfig.java
@@ -39,7 +39,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/api/v1/roles/select-role").permitAll()
-                        .requestMatchers("/chat").permitAll()
                         .anyRequest().authenticated()
                 )
 
