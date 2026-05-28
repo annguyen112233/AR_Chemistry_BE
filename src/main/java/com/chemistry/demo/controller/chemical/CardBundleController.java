653d7dc27823185294c74db69d958c312c3aba9c
Nguyen Hoai An <annguyen@gmail.com>
2026-05-28 11:59:39 +0700

    save

--- src/main/java/com/chemistry/demo/controller/chemical/CardBundleController.java
+++ src/main/java/com/chemistry/demo/controller/chemical/CardBundleController.java
@@ -3,18 +3,14 @@ package com.chemistry.demo.controller.chemical;
 import com.chemistry.demo.dto.ApiResponse;
 import com.chemistry.demo.dto.PageResponse;
 import com.chemistry.demo.dto.response.chemical.CardBundleResponse;
-import com.chemistry.demo.dto.response.chemical.ChemicalCardResponse;
 import com.chemistry.demo.services.chemical.CardBundleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
