ca01169b5a512e80b724711b900322acc9d18768
Nguyen Hoai An <annguyen@gmail.com>
2026-07-03 16:19:54 +0700

    fix: fix api billing

--- src/main/java/com/chemistry/demo/controller/payment/GooglePlayBillingController.java
+++ src/main/java/com/chemistry/demo/controller/payment/GooglePlayBillingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/billing/google-play")
+@RequestMapping("/payments/google-play")
 @RequiredArgsConstructor
 public class GooglePlayBillingController {
 
