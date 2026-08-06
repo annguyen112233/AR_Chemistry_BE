27e3fa4fe673121dcdaa11ef0a2cda066deafe23
baonoob1011 <trandinhbao222@gmail.com>
2026-08-06 09:20:13 +0700

    up

--- src/main/java/com/chemistry/demo/controller/ArAssetController.java
+++ src/main/java/com/chemistry/demo/controller/ArAssetController.java
@@ -18,11 +18,11 @@ public class ArAssetController {
     @GetMapping("/latest")
     public ApiResponse<ArAssetResponse> getLatestArAssets() {
         String markerKey = "ar-assets/markers/marker_bundle_v1.zip";
-        String reactionKey = "ar-assets/reactions/reaction_bundle_v1.zip";
+        String reactionKey = "ar-assets/reactions/reaction_bundle_v2.zip";
 
         ArAssetResponse response = ArAssetResponse.builder()
                 .markerVersion(1)
-                .reactionVersion(1)
+                .reactionVersion(2)
                 .markerUrl(s3Service.generatePresignedGetUrl(markerKey))
                 .reactionUrl(s3Service.generatePresignedGetUrl(reactionKey))
                 .markerSizeBytes(s3Service.getObjectSize(markerKey))
